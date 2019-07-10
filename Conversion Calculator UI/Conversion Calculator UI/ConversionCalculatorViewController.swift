7b13d23b858118704b06379a20c9e4192c7736eb
mrbrianjiang1121 <byjiang@outlook.com>
2019-07-09 19:45:36 -0500

    Created a conversion calculator for this project

---
+++ Conversion Calculator UI/Conversion Calculator UI/ConversionCalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConversionCalculatorViewController.swift
+//  Conversion Calculator UI
+//
+//  Created by Brian Jiang on 7/9/19.
+//  Copyright © 2019 Brian Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ConversionCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
