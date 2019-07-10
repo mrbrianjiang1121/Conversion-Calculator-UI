004960d02c2e5e700b155b68998094cb1e26b215
mrbrianjiang1121 <byjiang@outlook.com>
2019-07-09 19:56:28 -0500

    Finished Conversion Calculator UI Project Part 1

--- Conversion Calculator UI/Conversion Calculator UI/ConversionCalculatorViewController.swift
+++ Conversion Calculator UI/Conversion Calculator UI/ConversionCalculatorViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ConversionCalculatorViewController: UIViewController {
+struct UnitConverter {
+    let label: String
+    let inputUnit: String
+    let outputUnit: String
+}
 
+class ConversionCalculatorViewController: UIViewController {
+    @IBOutlet weak var OutputDisplay: UITextField!
+    @IBOutlet weak var InputDisplay: UITextField!
+    
+    var conversions = [UnitConverter(label: "Fahrenheit to Celcius", inputUnit: "F°", outputUnit: "C°"),
+                       UnitConverter(label: "Celcius to Fahrenheit", inputUnit: "C°", outputUnit: "F°"),
+                       UnitConverter(label: "Miles to Kilometers", inputUnit: "mi", outputUnit: "km"),
+                       UnitConverter(label: "Kilometers to Miles", inputUnit: "km", outputUnit: "mi")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        OutputDisplay.text = conversions[0].outputUnit
+        InputDisplay.text = conversions[0].inputUnit
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func Converter(_ sender: Any) {
+        let menuOptions = UIAlertController(title: "Converter Menu", message: "Choose Converter", preferredStyle: UIAlertController.Style.actionSheet)
+        
+        for unitconverter in conversions {
+            menuOptions.addAction(UIAlertAction(title: unitconverter.label, style: UIAlertAction.Style.default, handler: {
+                (alertAction) -> Void in
+                self.InputDisplay.text = unitconverter.inputUnit
+                self.OutputDisplay.text = unitconverter.outputUnit
+            }))
+        }
+        
+        menuOptions.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        self.present(menuOptions, animated: true, completion: nil)
+    }
+}
+    
     
 
     /*
@@ -27,4 +61,4 @@ class ConversionCalculatorViewController: UIViewController {
     }
     */
 
-}
+
